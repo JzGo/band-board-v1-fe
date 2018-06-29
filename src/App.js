698dbdc3c9432406ef34ba47c24d65990cc15169
Jezreel <jezreel.go@gmail.com>
2018-06-29 13:35:24 -0700

    add welcome text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import Navigation from './Components/Navigation'
+import './App.css';
+import Navigation from './Components/Navigation';
+import Welcome from './Pages/Welcome';
 
 class App extends Component {
   render() {
     return (
         <div>
           <Navigation />
+          <Welcome />
         </div>
     );
   }
