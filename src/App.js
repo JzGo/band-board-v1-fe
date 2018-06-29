b24be117871b89b83e79881beafc5b02cafca7ab
Jezreel <jezreel.go@gmail.com>
2018-06-29 16:52:13 -0700

    add login form placeholder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Navigation from './Components/Navigation';
 import Welcome from './Pages/Welcome';
+import LoginForm from './Components/LoginForm';
 
 class App extends Component {
   render() {
     return (
+      <Router>
         <div>
           <Navigation />
-          <Welcome />
+          <Route exact path="/" component={ Welcome } />
+          <Route path="/login" component={ LoginForm } />
         </div>
+      </Router>
     );
   }
 }
